761fad328308a72a3acec799d15c898099755c20
swifty2020 <unratedtech@gmail.com>
2020-06-29 16:07:47 -0400

    Initial Commit

---
+++ WOD/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WOD
+//
+//  Created by Tech on 6/29/20.
+//  Copyright © 2020 Tech. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
